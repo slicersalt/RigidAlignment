slicersalt/RigidAlignment

commit 7f0ac80749ab088dcf327a2bc8d9a98444005844

BUG: Fix SPHARM file names

RigidAlignment.cxx
@@ -178,7 +178,7 @@ void convertLandmarksToID(std::map<std::string, std::vector<int> > &landmarksMap
             std::string filename = meshList[i].substr(pivot);
 
             std::string name;
-            std::string suffixe_procalign = "_pp_surfSPHARM_procalign.vtk", suffixe_surfSPHARM = "_pp_surfSPHARM.vtk";
+            std::string suffixe_procalign = "_pp_surf_SPHARM_procalign.vtk", suffixe_surfSPHARM = "_pp_surf_SPHARM.vtk";
             int suffixe_size;
 
             if ( filename.substr(filename.length() - suffixe_surfSPHARM.length()) == suffixe_surfSPHARM )

commit dacd7ca503e3cf7a4a3301e5901e756acc978cca

COMP: Fix RigidAlignement build error on unix due to invalid array
This commit fixes the following build error introduced by 7f0ac80(BUG:
Fix Windows crash on array allocation):

  In file included from /usr/include/c++/5/vector:62:0,
                   from ./RigidAlignmentCLP.h:8,
                   from /path/to/SlicerSALT/RigidAlignment/RigidAlignment.cxx:11:
  /usr/include/c++/5/bits/stl_construct.h: In instantiation of ‘void std::_Destroy(_Tp*) [with _Tp = std::__cxx11::basic_string<char> [250]]’:
  /usr/include/c++/5/bits/stl_construct.h:103:19:   required from ‘static void std::_Destroy_aux<<anonymous> >::__destroy(_ForwardIterator, _ForwardIterator) [with _ForwardIterator = std::__cxx11::basic_string<char> (*)[250]; bool <anonymous> = false]’
  /usr/include/c++/5/bits/stl_construct.h:127:11:   required from ‘void std::_Destroy(_ForwardIterator, _ForwardIterator) [with _ForwardIterator = std::__cxx11::basic_string<char> (*)[250]]’
  /usr/include/c++/5/bits/stl_construct.h:151:15:   required from ‘void std::_Destroy(_ForwardIterator, _ForwardIterator, std::allocator<_T2>&) [with _ForwardIterator = std::__cxx11::basic_string<char> (*)[250]; _Tp = std::__cxx11::basic_string<char> [250]]’
  /usr/include/c++/5/bits/stl_vector.h:424:22:   required from ‘std::vector<_Tp, _Alloc>::~vector() [with _Tp = std::__cxx11::basic_string<char> [250]; _Alloc = std::allocator<std::__cxx11::basic_string<char> [250]>]’
  /path/to/SlicerSALT/RigidAlignment/RigidAlignment.cxx:239:54:   required from here
  /usr/include/c++/5/bits/stl_construct.h:93:7: error: request for member ‘~std::__cxx11::basic_string<char> [250]’ in ‘* __pointer’, which is of non-class type ‘std::__cxx11::basic_string<char> [250]’
       { __pointer->~_Tp(); }
         ^
  ninja: build stopped: subcommand failed.
  FAILED: slicersources-build/RigidAlignment-prefix/src/RigidAlignment-stamp/RigidAlignment-build

RigidAlignment.cxx
@@ -236,7 +236,7 @@ void getIDlandmarks(std::string mesh, std::string landmarks, std::vector<int> &l
   // Get the Surface filename from the command line
   std::fstream fcsvfile(landmarks.c_str());
   std::string line, mot;
-  std::vector< std::string[NB_LINES] > words(NB_WORDS); // !!!! WARNING DEFINE AND TO PROTECT IF SUPERIOR TO 20
+  std::vector< std::vector< std::string > > words(NB_LINES); // !!!! WARNING DEFINE AND TO PROTECT IF SUPERIOR TO 20
   int i,j, NbPoints;
 
   if(fcsvfile)
@@ -254,7 +254,7 @@ void getIDlandmarks(std::string mesh, std::string landmarks, std::vector<int> &l
     i=0;
     do
     {
-
+      words[i].resize(NB_WORDS);
       std::size_t pos_end;// = mot.find(",,");
       std::size_t pos1;
       j=0;